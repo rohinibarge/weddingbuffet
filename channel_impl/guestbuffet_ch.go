5f67b552a22dee6b17462952206f9b9460526620
Rohini Barge <rohini.pawar11@gmail.com>
2024-03-29 01:31:24 +0530

    demonstrate buffet management with channel and waitgroup

--- channel_impl/guestbuffet_ch.go
+++ channel_impl/guestbuffet_ch.go
@@ -21,26 +21,34 @@ type Guest struct {
 type semaphore chan struct{}
 
 func BuffetStart(plist []Guest) {
-
-	guests := make(chan int, len(plist))
-	finishStatus := make(chan int, len(plist))
+	// chanel of type pointer to guest objects of size 100
+	eat := make(chan *Guest, len(plist))
+	// chanel of type int of size 100
+	guestIds := make(chan int, len(plist))
 
 	buffetStarttime = time.Now()
 	fmt.Println("buffet Start time", buffetStarttime)
 
+	// we have 20 spoons, so only 20 guest can have dinner together at any time.
+	// so 20 goroutines will be running at any given time until we all guest have their food.
+	// startEating func called using goroutine, will wait for recive guest on channel eat
 	for i := 0; i <= SPOON; i++ {
-		go startEating(plist[i], guests, finishStatus)
+		go startEating(eat, guestIds)
 	}
 
-	for j := 1; j <= len(plist); j++ {
-		guests <- j
+	// write on eat channel, looping over guest list and send user to eat one by one
+	for j := 0; j < len(plist); j++ {
+		eat <- &plist[j]
 	}
-	close(guests)
+	close(eat)
 
+	// waiting for guests to finish their food.
+	// reading channel guestids, where we will recive guest ids who have finished their food.
 	for a := 1; a <= len(plist); a++ {
-		<-finishStatus
+		<-guestIds
 	}
 
+	// calculate total time taken
 	var totalwait time.Duration
 	for _, p := range plist {
 		totalwait = p.Waittime + time.Duration(totalwait)
@@ -50,8 +58,13 @@ func BuffetStart(plist []Guest) {
 	fmt.Println("buffet End time", time.Now())
 	fmt.Printf("All guests have eaten : %s ", time.Now().Sub(buffetStarttime))
 }
-func startEating(g Guest, guest <-chan int, finishStatus chan<- int) {
-	for i := range guest {
+
+// in this func which is called using goroutine, we will keep reading channel guest
+// we will recieve pointer to object of guest who were sent to have food.
+// func calculate wait time and completion time
+// and finally write guest id on guestIds channel who have finished their food.
+func startEating(guest <-chan *Guest, guestIds chan<- int) {
+	for g := range guest {
 		g.Starttime = time.Now()
 		g.Waittime = g.Starttime.Sub(buffetStarttime)
 		time.Sleep(time.Duration(g.Eattime) * time.Second)
@@ -59,7 +72,7 @@ func startEating(g Guest, guest <-chan int, finishStatus chan<- int) {
 
 		fmt.Printf("user %d waited upto %s seconds and finished eating in %d", g.Id, g.Waittime, g.Eattime)
 		fmt.Println(" ")
-		finishStatus <- i
+		guestIds <- g.Id
 	}
 
 }
